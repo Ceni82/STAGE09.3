bf5c9b1e27f4c17b84f499f0cf580ab56a0048c0
Mateus Ceni <mateus.ceni@gmail.com>
2023-05-02 19:21:09 -0300

    aula 3

--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -1,11 +1,16 @@
 import { Container } from './style';
 
+import { Button } from "../../components/Button"
 
 export function Details () {
   
   return (
     <Container>
       <h1>Hello World!!</h1>
+      <Button title="Login" loading />
+      <Button title= "Cadastrar"/>
+      <Button title={10}/>
+  
     </Container>
     
   )
