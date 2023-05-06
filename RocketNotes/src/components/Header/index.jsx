114c08fe1ca7d6cfd9b8bd47f99a9abb94afb3be
Mateus Ceni <mateus.ceni@gmail.com>
2023-05-05 22:03:10 -0300

    aula 4

---
+++ RocketNotes/src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+
+import { RiShutDownLine} from 'react-icons/ri'
+import { Container, Profile, Logout  } from "./style";
+
+export function Header() {
+    return(
+        <Container>
+            <Profile>
+                <img 
+                src= "https://media.licdn.com/dms/image/D4D03AQFYWajIIFqgxQ/profile-displayphoto-shrink_200_200/0/1663113811330?e=1688601600&v=beta&t=RzRmg-G0v2uQtAVEyFrt3FlPcexkY__w4eTSKqvxOG8"
+                alt = "Eu"
+                /> 
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Mateus Ceni</strong>
+                </div>
+            </Profile>
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
+            
+        </Container>
+    )
+
+}
--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -1,16 +1,48 @@
-import { Container } from './style';
+import { Container, Links, Content} from './style';
 
-import { Button } from "../../components/Button"
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
+ 
 export function Details () {
   
   return (
     <Container>
-      <h1>Hello World!!</h1>
-      <Button title="Login" loading />
-      <Button title= "Cadastrar"/>
-      <Button title={10}/>
-  
+      <Header />
+      <main>
+        <Content>
+
+            <ButtonText title="Excluir" />
+
+            <h1>Introdução ao React</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, ea aspernatur! 
+              Expedita animi magnam esse, assumenda obcaecati iste Lorem ipsum dolor, sit amet 
+              consectetur adipisicing elit. Perspiciatis at 
+              veniam vitae a quas mollitia veritatis odit voluptas quae placeat neque necessitatibus eius 
+              quisquam corrupti debitis, quos odio? Eaque, exercitationem? eum alias, non officiis saepe, 
+              corrupti enim! Saepe quae sequi commodi asperiores?
+            </p>
+
+            <Section title="Links úteis">
+              <Links>
+                <li><a href='#'>Link de um site que não existe</a></li>
+                <li><a href='#'>Link de outro site que não existe</a></li>
+                
+              </Links>
+
+            </Section>
+
+            <Section title="Marcadores">
+                <Tag title="express"/>
+                <Tag title="node.js"/>
+            </Section>
+            <Button title= "Voltar" />
+        </Content>
+      </main>
     </Container>
     
   )
